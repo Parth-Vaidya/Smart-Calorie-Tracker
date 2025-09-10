4bbf023f5c74a6be57bcf1fe52961f4882d510cd
BornToDie4u <lucifer25052005@gmail.com>
2025-09-10 23:12:18 +0530

    Implemenation of Tracker class creating helper function for getting date time and converting string to lower

--- SCT.cpp
+++ SCT.cpp
@@ -1,6 +1,74 @@
 #include<iostream>
+#include<map>
+#include <fstream>
+#include <sstream>
+#include <iomanip> 
+#include <limits>
+#include <algorithm>
+#include<vector>
+#include<ctime> 
+
 using namespace std;
-int main()
-{
-int a = 10;
+
+class IDataManager {
+    // Interface for data handling
+};
+
+class User{
+    // User data
+};
+
+class FoodItem{
+    // Food data
+};
+
+class Tracker {
+private:
+    IDataManager *dataManager;
+    map<string, User> users;
+    User * currentUser;
+    vector<FoodItem> foodDatabase;
+
+public: 
+
+    // this is a helper function should be shifted to the private section
+    string getDate() {
+        time_t t = time(0);
+        tm* now = localtime(&t);
+
+        stringstream ss;
+        ss << (now->tm_year + 1900) << '-'
+           << setfill('0') <<setw(2) << (now->tm_mon + 1) << '-'
+           << setfill('0') <<setw(2) << now->tm_mday;
+        return ss.str();
+    }
+
+    string toLower(string s){
+        transform(s.begin() ,s.end() , s.begin() ,:: tolower);
+        return s;
+
+    }
+
+
+    
+};
+
+int main() {
+//time implementation
+
+    Tracker t;
+    string date = t.getDate();
+    cout << date;
+
+
+// string to lower implementation
+
+    string s = "AadfdfbhhJHHJLHL gdsfgsdgsd";
+
+    string lower = t.toLower(s);
+    cout<<lower;
+
+//
+
+    return 0; 
 }
